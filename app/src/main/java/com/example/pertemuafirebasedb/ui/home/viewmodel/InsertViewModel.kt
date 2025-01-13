31271cc4f027a27d9393395b08348ce7e758d640
RezaAzharii <rezaazhari867@gmail.com>
2025-01-13 10:12:05 +0700

    membuat data class mahasiswaEvent

---
+++ app/src/main/java/com/example/pertemuafirebasedb/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.pertemuafirebasedb.ui.home.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuafirebasedb.model.Mahasiswa
+import com.example.pertemuafirebasedb.repository.RepositoryMhs
+import kotlinx.coroutines.launch
+
+
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    jenisKelamin = jenisKelamin,
+    alamat = alamat,
+    kelas = kelas,
+    angkatan = angkatan
+)
