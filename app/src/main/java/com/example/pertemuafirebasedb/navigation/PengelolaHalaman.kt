b6e2ffbc5d513772a02c4d7b488b063ee2ca5e5d
RezaAzharii <rezaazhari867@gmail.com>
2025-01-13 10:36:53 +0700

    menambahkan fungsi composable PengelolaHalman untuk mengatur navigasi antara HomeScreen dan InsertView menggunakan NavHost

---
+++ app/src/main/java/com/example/pertemuafirebasedb/navigation/PengelolaHalaman.kt
@@ -0,0 +1,41 @@
+package com.example.pertemuafirebasedb.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuafirebasedb.ui.home.pages.HomeScreen
+import com.example.pertemuafirebasedb.ui.home.pages.InsertView
+
+@Composable
+fun PengelolaHalman(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                onDetailClick = {
+
+                },
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                }
+            )
+        }
+        composable(DestinasiInsert.route){
+            InsertView(
+                onBack = {navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
